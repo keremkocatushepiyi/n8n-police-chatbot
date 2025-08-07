90cc69980039eba9a953f6ea91c334bc1204147d
keremkocatushepiyi <kerem.kocatus@hepiyi.com.tr>
2025-08-07 16:10:39 +0300

    Revert "first push"
    This reverts commit 1a871210065c6cce1206ae14d45ddd2d3eaa7327.

--- controllers/chat_controller.py
+++
@@ -1,6 +0,0 @@
-# controllers/chat_controller.py
-from services.webhook_service import send_to_n8n
-
-async def handle_prompt(prompt: str, user_id: str) -> str:
-    return await send_to_n8n(prompt, user_id)
-
--- core/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-N8N_WEBHOOK_URL = os.getenv("N8N_WEBHOOK_URL")
